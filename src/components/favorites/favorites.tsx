61dd96788a94ed5da29e16b45747fcd90552b61c
SemperFenix <dureivan@gmail.com>
2023-02-20 20:18:16 +0100

    Upddate favorites

--- src/components/favorites/favorites.tsx
+++ src/components/favorites/favorites.tsx
@@ -1,3 +1,19 @@
+import { useContext } from 'react';
+import { PhotosContext } from '../../context/photos.context';
+import { Card } from '../../components/card/card';
+import { MarsPhotoStructure } from '../../models/marsPhoto';
+import './favorites.scss';
+
 export function Favorites() {
-  return <h2>Favorites</h2>;
+  const { state } = useContext(PhotosContext);
+
+  return (
+    <>
+      <main className="favorites">
+        {state?.privatePhotos.map((item: MarsPhotoStructure) => (
+          <Card info={item} key={item.id}></Card>
+        ))}
+      </main>
+    </>
+  );
 }
