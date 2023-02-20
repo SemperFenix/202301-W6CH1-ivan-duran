9737e20273670532d9486d24dbf5063382388679
SemperFenix <dureivan@gmail.com>
2023-02-20 19:05:47 +0100

    Add favorites component and tesets

--- src/pages/favorites/favorites.test.tsx
+++ src/pages/favorites/favorites.test.tsx
@@ -1,12 +1,15 @@
-import { render, screen } from '@testing-library/react';
+import { act, render, screen } from '@testing-library/react';
 import FavoritesPage from './favorites';
 
 describe('Given the favorites page', () => {
   describe('When it is render', () => {
-    test('Then it should return a heading', () => {
+    test('Then it should return a heading', async () => {
       render(<FavoritesPage></FavoritesPage>);
-      const element = screen.getByRole('heading');
-      expect(element).toBeInTheDocument();
+
+      act(async () => {
+        const element = await screen.findByRole('img');
+        expect(element).toBeInTheDocument();
+      });
     });
   });
 });
--- src/pages/gallery/gallery.test.tsx
+++ src/pages/gallery/gallery.test.tsx
@@ -5,9 +5,9 @@ import GalleryPage from './gallery';
 describe('Given the new item page', () => {
   describe('When it is render', () => {
     test('Then it should return a heading', async () => {
-      // eslint-disable-next-line testing-library/no-unnecessary-act
+      render(<GalleryPage></GalleryPage>);
+
       act(async () => {
-        render(<GalleryPage></GalleryPage>);
         const element = await screen.findByRole('img');
         expect(element).toBeInTheDocument();
       });
