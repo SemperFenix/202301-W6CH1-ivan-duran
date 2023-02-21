247511045899044f6dba35fd0c2be4860c9ecf79
SemperFenix <dureivan@gmail.com>
2023-02-21 09:00:00 +0100

    Remove unnecessary import

--- src/components/new.item/new.item.test.tsx
+++ src/components/new.item/new.item.test.tsx
@@ -1,5 +1,4 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import { PhotosContext } from '../../context/photos.context';
 import { mockContext } from '../../mocks/tests.mocks';
 import { NewItem } from './new.item';
